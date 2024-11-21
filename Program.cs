63a262ac01b86f534474d1c97c8a06c0248a634f
Никита Паутов <145455384+shamans2321@users.noreply.github.com>
2024-11-21 10:43:22 +0300

    zd-3

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pp = @"C:\Users\Никита\Desktop\files.txt";
+            File.Create(pp).Close();
+            File.WriteAllText(pp, "я люблю лис");
+        }
+    }
+}
